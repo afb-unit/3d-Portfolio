d53a4ce986161569936faaf78b9189db0d92adcc
afb-unit <atbm7097@gmail.com>
2023-01-12 20:01:30 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+var typingeffect = new Typed(".multitext", {
+    strings: ["Λᶠ Ꮦ Λß", "a Coder", "a Designer"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backdelay: 1500
+  })
+ var loader = document.getElementById("preloader");
+ window.addEventListener("load", function(){
+   loader.style.display = "none";
+ })
+
+ let popup = document.getElementById("popup");
+ function openpopup() {
+     popup.classList.add("open-popup");
+ }
+ function closepopup() {
+     popup.classList.remove("open-popup");
+ }
+ var menulist = document.getElementById("menulistt");
+ menulist.style.maxHeight = "0px";
+ function togglemenu(){
+  if(menulist.style.maxHeight== "0px")
+  {
+    menulist.style.maxHeight= "165px";
+ }
+ else{
+  menulist.style.maxHeight= "0px";
+ }
+}
+
+var menulist = document.getElementById("menulistt");
+ menulist.style.maxHeight = "0px";
+ function tog(){
+  if(menulist.style.maxHeight== "0px")
+  {
+    menulist.style.maxHeight= "165px";
+ }
+ else{
+  menulist.style.maxHeight= "0px";
+ }
+}
+const audio = new Audio();
+audio.src = "./assets/glass sound effect.mp3";
