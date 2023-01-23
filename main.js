39293be2eb1c1ab85138268c15b454ef12a9184d
afb-unit <121726327+afb-unit@users.noreply.github.com>
2023-01-23 10:10:33 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,50 +1,72 @@
-var typingeffect = new Typed(".multitext", {
+//preloader
+var loader = document.getElementById("preloader");
+window.addEventListener("load", function(){
+ loader.style.display = "none";
+})
+//extra time pre
+setTimeout(function(){
+    var msg = document.getElementById("loading-sp1");
+    msg.parentNode.removeChild(msg);
+    },45000
+    );
+  //start
+  document.addEventListener('DOMContentLoaded', () => {
+    const section = document.querySelector('#section1');
+    const button= section.querySelector('.maskbt');
+    button.addEventListener('click', () => {
+      section.classList.add('transition','animate');
+      setTimeout(
+        ()=> {
+          section.classList.add('animate-completed');
+        },1000
+      )
+    })
+  });
+  //auto type
+  var typingeffect = new Typed(".multitext", {
     strings: ["Λᶠ Ꮦ ԹՅ", "a Coder", "a Designer"],
     loop: true,
     typeSpeed: 100,
     backSpeed: 80,
     backdelay: 1500
   })
- var loader = document.getElementById("preloader");
- window.addEventListener("load", function(){
-   loader.style.display = "none";
- })
 
- let popup = document.getElementById("popup");
- function openpopup() {
-     popup.classList.add("open-popup");
- }
- function closepopup() {
-     popup.classList.remove("open-popup");
- }
- var menulist = document.getElementById("menulistt");
- menulist.style.maxHeight = "0px";
- function togglemenu(){
-  if(menulist.style.maxHeight== "0px")
-  {
-    menulist.style.maxHeight= "165px";
- }
- else{
-  menulist.style.maxHeight= "0px";
- }
+//three line burger
+var menulist = document.getElementById("nav-links");
+menulist.style.maxHeight = "0vh";
+function togglemenu(){
+if(menulist.style.maxHeight== "0vh")
+{
+  menulist.style.maxHeight= "90vh";
+}
+else{
+menulist.style.maxHeight= "0vh";
+}
 }
 
-var menulist = document.getElementById("menulistt");
- menulist.style.maxHeight = "0px";
- function tog(){
-  if(menulist.style.maxHeight== "0px")
-  {
-    menulist.style.maxHeight= "165px";
- }
- else{
-  menulist.style.maxHeight= "0px";
- }
+var menulist = document.getElementById("nav-links");
+menulist.style.maxHeight = "0px";
+function tog(){
+if(menulist.style.maxHeight== "0px")
+{
+  menulist.style.maxHeight= "90vh";
 }
+else{
+menulist.style.maxHeight= "0px";
+}
+}
+
+
+//audio
 const audio = new Audio();
 audio.src = "./assets/glass sound effect.mp3";
 
-setTimeout(function(){
-  var msg = document.getElementById("loading-sp");
-  msg.parentNode.removeChild(msg);
-},45000
-  );
+//form popup
+let popup = document.getElementById("popup");
+function openpopup() {
+    popup.classList.add("open-popup");
+}
+function closepopup() {
+    popup.classList.remove("open-popup");
+}
+
